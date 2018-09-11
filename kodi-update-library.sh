27616b36caefd6f4b897c640e6a79f8109d23b3c
Nacho Barrientos <nacho.barrientos@cern.ch>
2018-09-11 22:29:03 +0200

    Use kodi_library plugin instead of a custom script

--- kodi-update-library.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-/usr/bin/wget --header='Content-Type:application/json' --post-data='{"jsonrpc": "2.0", "method": "VideoLibrary.Scan", "id": "xbian"}' -O /dev/null "http://localhost/jsonrpc"
